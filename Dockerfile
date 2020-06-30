69cea8bb1b549e35a8e45fe362b142b763b9c416
Loongel <code@loongel.tech>
2020-06-30 13:26:26 +0800

    ossfs coding
    The  all project files were modified

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM golang:1.14.4-alpine3.12 as builder
-COPY . /go/src/github.com/fentas/docker-volume-ossfs
-WORKDIR /go/src/github.com/fentas/docker-volume-ossfs
+COPY . /go/src/github.com/loongel/docker-volume-ossfs
+WORKDIR /go/src/github.com/loongel/docker-volume-ossfs
 RUN set -ex \
     && apk add --no-cache --virtual .build-deps \
     gcc libc-dev \
@@ -9,19 +9,19 @@ RUN set -ex \
 CMD ["/go/bin/docker-volume-ossfs"]
 
 FROM alpine:3.12
-RUN apk add --no-cache ossfs2
+RUN apk add --no-cache fuse alpine-sdk automake autoconf libxml2-dev fuse-dev curl-dev \
+  && wget -qO - https://github.com/aliyun/ossfs/archive/v1.80.6.zip |unzip -q - \
+  && mv ossfs-1.80.6 ossfs-master \
+  && cd ossfs-master \
+  && sh ./autogen.sh \
+  && sh ./configure --prefix=/usr \
+  && make \
+  && make install \
+  && rm -rf /var/cache/apk/* \
+  && cd .. && rm -rf ossfs-master
 RUN mkdir -p /run/docker/plugins /mnt/state /mnt/volumes
-RUN echo -e $'\
-dav_user        root\n\
-dav_group       root\n\
-kernel_fs       fuse\n\
-buf_size        16\n\
-connect_timeout 10\n\
-read_timeout    30\n\
-retry           10\n\
-max_retry       300\n\
-dir_refresh     30\n\
-# file_refresh    10\n\
-' >> /etc/ossfs2/ossfs2.conf
+
 COPY --from=builder /go/bin/docker-volume-ossfs .
 CMD ["docker-volume-ossfs"]
+
+
--- main.go
+++ main.go
@@ -1,367 +1,199 @@
 package main
 
 import (
-	"crypto/md5"
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
 	"log"
-	"net/url"
+	"flag"
+	"fmt"
 	"os"
-	"os/exec"
-	"path/filepath"
-	"strconv"
+	"regexp"
+	"context"
 	"strings"
-	"sync"
-
-	"github.com/Sirupsen/logrus"
+	"bufio"
+	//"github.com/go-ini/ini"
+	"github.com/moby/moby/client"
 	"github.com/docker/go-plugins-helpers/volume"
 )
-
 const socketAddress = "/run/docker/plugins/ossfs.sock"
+const defaultVolumeRoot = "/mnt/volumes"	// different from volume.DefaultDockerRootDirectory, used by linked mount path (ALiOssVolumeDriver.mount). 
+											// It's different from "ossfsRoot", I don't know why it is needed, maybe I will retain just one of the two vars later.
+var (
+	mount  = defaultVolumeRoot	
+	config = flag.String("config", "", "a config file or a config variant name of swarm about alyun oss infomation, for example:\n" +
+		"	ali-oss-vd -config /var/lib/ossfs/def.conf	-->config file\n" +
+		"	ali-oss-vd -config conf.oss.driver.volume	-->config variant in docker swarm\n" +
+		"whether a file or a variant in docker swarm, the content may be like this:\n" +
+		"	-------------------------------------------------------------\n" +
+		"	[M1]			# You can name ossfs connection as M1\n" +
+		"	endpoint=AAA1		# ossfs EndPoint\n" +
+		"	accesskeyid=AAA2	# ossfs AccessKeyId\n" +
+		"	accesskeysecret=AAA3	# ossfs AccessKeySecret\n" +
+		"\n" +
+		"	[M2]\n" +
+                "	endpoint=BBB1\n" +
+                "	accesskeyid=BBB2\n" +
+                "	accesskeysecret=BBB3\n" +
+		"	-------------------------------------------------------------\n" +
+		"\n\n" +
+		"volume use like this:\n" +
+		"	docker volume create --name <VOLUME-NAME> -d ali-oss-vd -o name-ref=<M1 OR M2> -o bucket=<BUCKET-NAME> -o path=<PATH IN BUCKET>\n" +
+		"	docker run -it --name Test -v <VOLUME-NAME>:/data --volume-driver=ali-oss-vd alpine\n" +
+		"or direct like this:\n" +
+		"	docker run -it --name Test -v <VOLUME-NAME>[name-ref=<M1 OR M2>,bucket=<BUCKET-NAME>,path=<PATH IN BUCKET>]:/data --volume-driver=ali-oss-vd alpine")
+	debug  = flag.Bool("debug", false, "print debug info on console with value true")
+)
 
-type ossfsVolume struct {
-	URL      string
-	Username string
-	Password string
-	Conf     string
-	UID      string
-	GID      string
-	FileMode string
-	DirMode  string
-	Ro       bool
-	Rw       bool
-	Exec     bool
-	Suid     bool
-	Grpid    bool
-	Netdev   bool
-
-	Mountpoint  string
-	Md5str string
-	connections int
-}
-
-type ossfsDriver struct {
-	sync.RWMutex
-
-	root      string
-	statePath string
-	volumes   map[string]*ossfsVolume
-}
-
-func newossfsDriver(root string) (*ossfsDriver, error) {
-	logrus.WithField("method", "new driver").Debug(root)
-
-	d := &ossfsDriver{
-		root:      filepath.Join(root, "volumes"),
-		statePath: filepath.Join(root, "state", "ossfs-state.json"),
-		volumes:   map[string]*ossfsVolume{},
+//type OssDef map[string]string
+//var ossDefItems = []string{"endpoint", "accesskeyid", "accesskeysecret"}
+func main() {
+	drvn := socketAddress	
+	var Usage = func() {
+		fmt.Printf("Name: " + drvn + "\n")
+		fmt.Printf("Describe: a docker volume plugin driver for oss of aliyun based on ossfs \n")
+		fmt.Printf("Author: Loongel\n")
+		fmt.Printf("Note: this driver rely on ossfs driver of aliyun, install ossfs like this:\n" +
+		    "	read https://github.com/aliyun/ossfs Section：install\n " +"\n"+
+			"	wget http://docs-aliyun.cn-hangzhou.oss.aliyun-inc.com/assets/attach/32196/cn_zh/1496671412523/ossfs_1.80.2_centos7.0_x86_64.rpm\n" +
+			"	yum localinstall ossfs_1.80.2_centos7.0_x86_64.rpm\n" +
+			"just this is satisfy with current docker volume plugin! if you want to use it independent in host, you need to do this:\n" +
+			"	echo <BUCKET-NAME>:<AccessKeyId>:<AccessKeySecret> > /etc/passwd-ossfs\n" +
+			"	chmod 640 /etc/passwd-ossfs\n" +
+			"	ossfs <PATH IN BUCKET] <MOUNTPOINT> -ourl=<EndPoint>		--->mount\n" +
+			"	fusermount -u <MOUNTPOINT>					--->ummount\n\n")
+		fmt.Fprintf(os.Stderr, "Usage: %s [options]\n", os.Args[0])
+		flag.PrintDefaults()
 	}
-
-	data, err := ioutil.ReadFile(d.statePath)
-	if err != nil {
-		if os.IsNotExist(err) {
-			logrus.WithField("statePath", d.statePath).Debug("no state found")
-		} else {
-			return nil, err
-		}
-	} else {
-		if err := json.Unmarshal(data, &d.volumes); err != nil {
-			return nil, err
-		}
+	var Excute = func(dfs map[string]OssDef){
+		dbg := false
+		if *debug {
+			dbg = true
+		}		
+	    	//driver := NewALiOssVolumeDriver(mount, drvn, dfs, dbg)
+			driver := NewALiOssVolumeDriver(mount, drvn, dbg)
+	        handler := volume.NewHandler(driver)
+    	        if err := handler.ServeUnix(drvn, 0); err != nil {
+    	                log.Fatalf("Error %v", err)
+    	        }
 	}
-
-	return d, nil
-}
-
-func (d *ossfsDriver) saveState() {
-	data, err := json.Marshal(d.volumes)
-	if err != nil {
-		logrus.WithField("statePath", d.statePath).Error(err)
+	flag.Parse();
+	ots, _ := ExecuteCmd("type ossfs >/dev/null 2>&1 || { echo >&2 \"none\";}", 1, *debug)
+	if strings.Contains(ots, "none"){
+		fmt.Printf("%c[1;0;31merror: ossfs not installed!%c[0m\n",0x1B, 0x1B)
+		Usage()
 		return
 	}
 
-	if err := ioutil.WriteFile(d.statePath, data, 0644); err != nil {
-		logrus.WithField("savestate", d.statePath).Error(err)
-	}
-}
-
-func (d *ossfsDriver) Create(r *volume.CreateRequest) error {
-	logrus.WithField("method", "create").Debugf("%#v", r)
-
-	d.Lock()
-	defer d.Unlock()
-	v := &ossfsVolume{}
-
-	for key, val := range r.Options {
-		switch key {
-		case "url":
-			v.URL = val
-		case "username":
-			v.Username = val
-		case "password":
-			v.Password = val
-		case "conf":
-			v.Conf = val
-		case "uid":
-			v.UID = val
-		case "gid":
-			v.GID = val
-		case "file_mode":
-			v.FileMode = val
-		case "dir_mode":
-			v.DirMode = val
-		case "ro":
-			v.Ro = true
-		case "rw":
-			v.Rw = true
-		case "exec":
-			v.Exec = true
-		case "suid":
-			v.Suid = true
-		case "grpid":
-			v.Grpid = true
-		case "_netdav":
-			v.Netdev = true
-		default:
-			return logError("unknown option %q", val)
-		}
-	}
-
-	if v.URL == "" {
-		return logError("'url' option required")
-	}
-	_, err := url.Parse(v.URL)
-	if err != nil {
-		return logError("'url' option malformed")
-	}
-	v.Mountpoint = filepath.Join(d.root, fmt.Sprintf("%x", md5.Sum([]byte(v.URL))))
-	v.Md5str = fmt.Sprintf("%x", md5.Sum([]byte(v.URL)))
-
-	d.volumes[r.Name] = v
-	d.saveState()
-
-	return nil
-}
-
-func (d *ossfsDriver) Remove(r *volume.RemoveRequest) error {
-	logrus.WithField("method", "remove").Debugf("%#v", r)
-
-	d.Lock()
-	defer d.Unlock()
-
-	v, ok := d.volumes[r.Name]
-	if !ok {
-		return logError("volume %s not found", r.Name)
-	}
-
-	if v.connections != 0 {
-		return logError("volume %s is currently used by a container", r.Name)
-	}
-	if err := os.RemoveAll(v.Mountpoint); err != nil {
-		return logError(err.Error())
-	}
-	delete(d.volumes, r.Name)
-	d.saveState()
-	return nil
-}
-
-func (d *ossfsDriver) Path(r *volume.PathRequest) (*volume.PathResponse, error) {
-	logrus.WithField("method", "path").Debugf("%#v", r)
-
-	d.RLock()
-	defer d.RUnlock()
-
-	v, ok := d.volumes[r.Name]
-	if !ok {
-		return &volume.PathResponse{}, logError("volume %s not found", r.Name)
-	}
-
-	return &volume.PathResponse{Mountpoint: v.Mountpoint}, nil
-}
-
-func (d *ossfsDriver) Mount(r *volume.MountRequest) (*volume.MountResponse, error) {
-	logrus.WithField("method", "mount").Debugf("%#v", r)
-
-	d.Lock()
-	defer d.Unlock()
-
-	v, ok := d.volumes[r.Name]
-	if !ok {
-		return &volume.MountResponse{}, logError("volume %s not found", r.Name)
-	}
-
-	if v.connections == 0 {
-		fi, err := os.Lstat(v.Mountpoint)
-		if os.IsNotExist(err) {
-			if err := os.MkdirAll(v.Mountpoint, 0755); err != nil {
-				return &volume.MountResponse{}, logError(err.Error())
-			}
-		} else if err != nil {
-			return &volume.MountResponse{}, logError(err.Error())
-		}
-
-		if fi != nil && !fi.IsDir() {
-			return &volume.MountResponse{}, logError("%v already exist and it's not a directory", v.Mountpoint)
-		}
-
-		if err := d.mountVolume(v); err != nil {
-			return &volume.MountResponse{}, logError(err.Error())
-		}
-	}
-	v.connections++
-
-	return &volume.MountResponse{Mountpoint: v.Mountpoint}, nil
-}
-
-func (d *ossfsDriver) Unmount(r *volume.UnmountRequest) error {
-	logrus.WithField("method", "unmount").Debugf("%#v", r)
-
-	d.Lock()
-	defer d.Unlock()
-	v, ok := d.volumes[r.Name]
-	if !ok {
-		return logError("volume %s not found", r.Name)
-	}
-
-	v.connections--
-
-	if v.connections <= 0 {
-		if err := d.unmountVolume(v.Mountpoint); err != nil {
-			return logError(err.Error())
-		}
-		v.connections = 0
-	}
-
-	return nil
-}
-
-func (d *ossfsDriver) Get(r *volume.GetRequest) (*volume.GetResponse, error) {
-	logrus.WithField("method", "get").Debugf("%#v", r)
-
-	d.Lock()
-	defer d.Unlock()
-
-	v, ok := d.volumes[r.Name]
-	if !ok {
-		return &volume.GetResponse{}, logError("volume %s not found", r.Name)
-	}
-
-	return &volume.GetResponse{Volume: &volume.Volume{Name: r.Name, Mountpoint: v.Mountpoint}}, nil
-}
-
-func (d *ossfsDriver) List() (*volume.ListResponse, error) {
-	logrus.WithField("method", "list").Debugf("")
-
-	d.Lock()
-	defer d.Unlock()
-
-	var vols []*volume.Volume
-	for name, v := range d.volumes {
-		vols = append(vols, &volume.Volume{Name: name, Mountpoint: v.Mountpoint})
-	}
-	return &volume.ListResponse{Volumes: vols}, nil
-}
-
-func (d *ossfsDriver) Capabilities() *volume.CapabilitiesResponse {
-	logrus.WithField("method", "capabilities").Debugf("")
-
-	return &volume.CapabilitiesResponse{Capabilities: volume.Capability{Scope: "local"}}
-}
-
-func (d *ossfsDriver) mountVolume(v *ossfsVolume) error {
-	logrus.WithField("method", "mountVolume").Debugf("%#v", v)
-
-	u, err := url.Parse(v.URL)
-	if err != nil {
-		log.Fatal(err)
-	}
-	logrus.WithField("method", "mountVolume").WithField("variable", "url").Debugf("%#v", u)
+	flag.Parse();
 	
-	cmd := exec.Command("mount.ossfs", fmt.Sprintf("%s://%s%s", u.Scheme, u.Host, u.Path), v.Mountpoint)
-
-	if v.Conf != "" {
-		cmd.Args = append(cmd.Args, "-o", fmt.Sprintf("conf=%s", v.Conf))
-	}
-	if v.UID != "" {
-		exec.Command("adduser", "-S", "-u", v.UID, v.UID).Run()
-		cmd.Args = append(cmd.Args, "-o", fmt.Sprintf("uid=%s", v.UID))
-	}
-	if v.GID != "" {
-		exec.Command("addgroup", "-S", "-g", v.GID, v.GID).Run()
-		cmd.Args = append(cmd.Args, "-o", fmt.Sprintf("gid=%s", v.GID))
-	}
-	if v.FileMode != "" {
-		cmd.Args = append(cmd.Args, "-o", fmt.Sprintf("file_mode=%s", v.FileMode))
-	}
-	if v.DirMode != "" {
-		cmd.Args = append(cmd.Args, "-o", fmt.Sprintf("dir_mode=%s", v.DirMode))
-	}
-	if v.Ro {
-		cmd.Args = append(cmd.Args, "-o", "ro")
-	}
-	if v.Rw {
-		cmd.Args = append(cmd.Args, "-o", "rw")
-	}
-	if v.Exec {
-		cmd.Args = append(cmd.Args, "-o", "exec")
-	}
-	if v.Suid {
-		cmd.Args = append(cmd.Args, "-o", "suid")
-	}
-	if v.Grpid {
-		cmd.Args = append(cmd.Args, "-o", "grpid")
-	}
-	if v.Netdev {
-		cmd.Args = append(cmd.Args, "-o", "_netdev")
-	}
+	// var conf *ini.File
+	// if len(*config) == 0{
+		// Usage()
+		// Excute(nil)
+		// return
+	// }
+	// f, err := os.Stat(*config)
+	// if err == nil || os.IsExist(err) {
+		// if f.IsDir() {
+			// fmt.Printf("%c[1;0;31merror: config \"%s\" must be a file!%c[0m\n",0x1B, *config, 0x1B)
+			// Usage()
+			// Excute(nil)
+			// return
+		// }
+  		// fcf, err := ini.Load(*config)
+     	        // if err != nil {
+     	      		// log.Fatal(err)
+			// Usage()
+     		        // Excute(nil)
+			// return
+     	        // }
+		// conf = fcf
+	// }else{
+		// r, _ := regexp.Compile("^[A-Za-z][A-Za-z0-9._-]*$")
+		// if !r.MatchString(*config) {
+			// fmt.Printf("%c[1;0;31merror: config \"%s\" is not a exist file and not a swarm config variant name!%c[0m\n",0x1B, *config, 0x1B)
+			// Usage()
+			// Excute(nil)
+			// return
+		// }
+		// cnt := 0
+// LABEL_AGAIN:
+		// cli, err := client.NewEnvClient()
+		// if err != nil {
+			// fmt.Printf("%c[1;0;31merror: config \"%s\" is a swarm config variant name, connect docker host fail!%c[0m\n",0x1B, *config, 0x1B)
+			// panic(err)
+			// Usage()
+			// Excute(nil)
+			// return
+		// }
+		// confspc, _, err := cli.ConfigInspectWithRaw(context.Background(), *config)
+		// if err != nil {
+			// fmt.Printf("%c[1;0;31merror: config \"%s\" is a swarm config variant name, get it's content from docker fail:%c[0m\n", 0x1B, *config, 0x1B)
+			// errTxt := err.Error();
+			// if strings.Index(errTxt, "is too new. Maximum supported API version is") != -1{
+				// reg := regexp.MustCompile(`\d+\.\d+$`)
+                                // ver := reg.FindAllString(errTxt, -1)
+				// fmt.Printf(errTxt + "\n")
+				// if cnt == 0 {
+					// cnt += 1
+					// fmt.Printf("	auto solve: execute \"export DOCKER_API_VERSION=%s\" first, then try again!\n", ver[0])
+					// os.Setenv("DOCKER_API_VERSION", ver[0])
+                                        // goto LABEL_AGAIN
+				// }
+				// fmt.Printf("%c[1;0;32mTo Solve: automatically match the current docker API version(export DOCKER_API_VERSION=%s)?[y/n]%c[0m", 0x1B, ver[0], 0x1B)
+				// scanner := bufio.NewScanner(os.Stdin)
+				// scanner.Scan()
+				// if err := scanner.Err(); err != nil {
+					// fmt.Fprintln(os.Stderr, "error:", err)
+					// Usage()
+					// Excute(nil)
+					// return
+				// }
+				// if scanner.Text() == "y" {
+					// os.Setenv("DOCKER_API_VERSION", ver[0])
+					// goto LABEL_AGAIN
+				// }
+			// }
+			// Usage()
+			// Excute(nil)
+			// return
+		// }
+		// bcf, err := ini.Load(confspc.Spec.Data)
+    		// if err != nil {
+                   	// log.Fatal(err)
+			// Usage()
+			// Excute(nil)
+			// return
+             	// }
+		// conf = bcf
+	// }
 	
-	logrus.Debug(cmd.Args)
+	// sts := conf.SectionStrings()
+	// dfs := make(map[string]OssDef)
+	// for _, sec := range sts {
+		// item := make(OssDef);
+		// cnt := 0
+		// for _, key := range ossDefItems{
+			// val := conf.Section(sec).Key(key).String();
+			// if val == "" {
+				// continue;
+			// }
+			// cnt += 1
+			// item[key] = val;			
+		// }
+		// if cnt == 0 {
+			// continue;
+		// }
+		// if cnt < len(ossDefItems) {
+			// fmt.Printf("oss define \"%s\"...parse...[error]\n", sec)
+			// continue
+		// }
+		// dfs[sec] = item
+		// fmt.Printf("oss define \"%s\"...parse...[ok]\n", sec);
+	// }
+	// if len(dfs) <= 0 {
+		// fmt.Printf("%c[1;0;32mwarnning: config \"%s\" has't any oss define!%c[0m\n",0x1B, *config, 0x1B)
+	// }
+	// Excute(dfs)
 	
-	pidFile := fmt.Sprintf("/var/run/mount.ossfs/mnt-volumes-%s.pid", v.Md5str)
-	mountCmd := cmd.String()
-	
-	cmd_full := exec.Command(fmt.Sprintf("kill -0 $(cat %s) > /dev/null 2>&1;if [ $? -ne 0 ];then rm %s -f && %s;fi", pidFile, pidFile, mountCmd))
-	
-	if u.User != nil {
-		username := u.User.Username()
-		password, _ := u.User.Password()
-		cmd_full.Stdin = strings.NewReader(fmt.Sprintf("%s\n%s", username, password))
-	} else if v.Username != "" {
-		cmd_full.Stdin = strings.NewReader(fmt.Sprintf("%s\n%s", v.Username, v.Password))
-	}
-				 
-	return cmd_full.Run()
-}
-
-func (d *ossfsDriver) unmountVolume(target string) error {
-	cmd := fmt.Sprintf("umount %s", target)
-	logrus.Debug(cmd)
-	return exec.Command("sh", "-c", cmd).Run()
-}
-
-func logError(format string, args ...interface{}) error {
-	logrus.Errorf(format, args...)
-	return fmt.Errorf(format, args)
-}
-
-func main() {
-	debug := os.Getenv("DEBUG")
-	if ok, _ := strconv.ParseBool(debug); ok {
-		logrus.SetLevel(logrus.DebugLevel)
-	}
-
-	// make sure "/etc/ossfs2/secrets" is owned by root
-	err := os.Chown("/etc/ossfs2/secrets", 0, 0)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	d, err := newossfsDriver("/mnt")
-	if err != nil {
-		log.Fatal(err)
-	}
-	h := volume.NewHandler(d)
-	logrus.Infof("listening on %s", socketAddress)
-	logrus.Error(h.ServeUnix(socketAddress, 0))
+	Excute(nil)
+	return
 }
